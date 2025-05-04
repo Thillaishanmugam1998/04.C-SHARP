226c6c0a60bfdcd4639cf0bcf65dd4ab7946d091
ThillaiShanmugam1998 <shansenthil72@gmail.com>
2025-05-04 09:34:09 +0530

    Added OOPS

---
+++ 3.OOPS/1.CLASS_OBJECTS/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _1.CLASS_OBJECTS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
